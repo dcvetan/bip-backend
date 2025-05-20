286dcdad4ba53d88ab479931b0b1884fca1c64ac
Dominik Cvetan <dominik.cvetan@gmail.com>
2025-05-20 12:07:07 +0200

    add test controller for render test

---
+++ src/main/kotlin/app/biptrip/bipbackend/api/TestController.kt
@@ -0,0 +1,17 @@
+package app.biptrip.bipbackend.api
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/test")
+class TestController {
+
+    @GetMapping
+    suspend fun verifyPurchase(): ResponseEntity<Unit> {
+        return ResponseEntity.ok().build()
+    }
+
+}
